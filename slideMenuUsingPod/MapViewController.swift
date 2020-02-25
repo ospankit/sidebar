c1518a93f46426ae271ae7598c6b9c364d997d21
ankit <ops.ankit80@gmail.com>
2020-02-25 20:16:50 +0530

    map feature added

---
+++ slideMenuUsingPod/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  slideMenuUsingPod
+//
+//  Created by Ankit on 25/02/20.
+//  Copyright © 2020 Ankit. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
